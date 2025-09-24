52ac98119fd78c0815da673664d8fd6fd4417ed3
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-09-25 03:02:35 +0500

    for...in and for..of

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const user = {
-    name: "Javohir",
-    age: 21,
-    firstName: "Suvonov"
-};
+const fruits = ["apple", "banana", "cherry"];
 
-for (key in user) {
-    console.log(key, ":", user[key])
+console.log("for...in")
+for(let i in fruits){
+    console.log(i , ":" , fruits[i])
+}
+
+console.log("for...of")
+for(let i of fruits){
+    console.log(i)
 }
