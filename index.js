ba57f2d06676e175c7351c203a985645cf884c10
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-09-25 17:21:59 +0500

    update

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-const myObj = {};
+const person = {
+    name: "Javohir",
+    firstName: "Suvonov",
+    get fullName() {
+        return this.name + " " + this.firstName;
+    },
+    set update(value) {
+        const arr = value.split(" ");
+        this.name = arr[0];
+        this.firstName = arr[1]
+    }
+}
 
-const color = "red";
-const key = "age"
-
-myObj.name = "Javohir";
-myObj.color = color;
-myObj[key] = 21;
-
-console.log(myObj)
+console.log(person.fullName);
+person.update =  "Ulug'bek Suvonov";
+console.log(person.fullName);
+console.log(person)
